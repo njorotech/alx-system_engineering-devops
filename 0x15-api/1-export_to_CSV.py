4e7c7b5002b71f00b7990dbee8d38221de393dbe
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-02-20 09:59:36 +0300

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+''' Export data in the CSV format.'''
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    emId = argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    usersResponse = requests.get(f'{url}/users/{emId}')
+    user = usersResponse.json()
+    name = user['name']
+
+    payload = {"userId": emId}
+    todo_response = requests.get(f'{url}/todos', params=payload)
+    todo = todo_response.json()
+
+    with open('USER_ID.csv', mode='w') as csv_file:
+        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS",
+                      "TASK_TITLE"]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames,
+                                quotechar='"', quoting=csv.QUOTE_ALL)
+
+        # writer.writeheader()
+
+        for item in todo:
+            if item['completed']:
+                status = 'True'
+            else:
+                status = 'False'
+            writer.writerow({'USER_ID': f'{emId}', 'USERNAME': f'{name}',
+                             'TASK_COMPLETED_STATUS': f'{status}',
+                             'TASK_TITLE': f'{item["title"]}'})
