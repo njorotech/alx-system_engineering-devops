c06edf46728d7f0dede9dd0dec782431eeec3620
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-02-22 09:50:37 +0300

    How many subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+'''
+Queries the Reddit API and returns the number of subscribers for a given
+subreddit. If an invalid subreddit is given, the function should return 0
+'''
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """Returns the number of subscribers to the subreddit"""
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {
+        'User-Agent': 'Muchoki'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    # print(response.status_code)
+    if response.status_code == 200:
+        json_data = response.json()
+        data_results = json_data.get("data")
+        subscribers = data_results.get("subscribers")
+        return subscribers
+    else:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
