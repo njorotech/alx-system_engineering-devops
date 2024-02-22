aa7226e495af9bcd253b29dcb2a8c6438e30b903
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-02-22 11:16:01 +0300

    Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+queries the Reddit API and prints the titles of the first 10 hot posts listed
+for a given subreddit.
+"""
+import requests
+import json
+
+
+def top_ten(subreddit):
+    """Prints titles of the first 10 hot posts"""
+
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=9'
+    headers = {
+        'User-Agent': 'Muchoki'
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    # print(response.status_code)
+    if response.status_code == 200:
+        json_data = response.json()
+        # print(json.dumps(json_data, indent=4))
+        for item in json_data["data"]["children"]:
+            print(item["data"]["title"])
+    else:
+        print("None")
