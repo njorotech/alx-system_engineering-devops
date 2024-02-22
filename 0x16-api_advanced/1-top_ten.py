27e9b60a1a1b06e2fb3080f3d424bd1a4d493191
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-02-22 17:53:01 +0300

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ def top_ten(subreddit):
     """Prints titles of the first 10 hot posts"""
 
     if subreddit is None or type(subreddit) is not str:
-        print(None)
+        return None
 
     url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=9'
     headers = {
@@ -19,12 +19,12 @@ def top_ten(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     # print(response.status_code)
     if response.status_code == 200:
-        if not response.content:
-            print(None)
+        json_data = response.json()
+        if not json_data["data"]["children"]:
+            return None
         else:
-            json_data = response.json()
             # print(json.dumps(json_data, indent=4))
             for item in json_data["data"]["children"]:
                 print(item["data"]["title"])
     else:
-        print(None)
+        return None
