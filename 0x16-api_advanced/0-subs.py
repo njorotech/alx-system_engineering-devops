764d72c22c74e485d0b242849f70e2c27aa149e6
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-03-05 16:52:59 +0300

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+'''
+Queries the Reddit API and returns the number of subscribers for a given
+subreddit. If an invalid subreddit is given, the function should return 0
+'''
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    """Returns the number of subscribers to the subreddit"""
+
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+
+    # if subreddit is None or not isinstance(subreddit, str):
+    #     return 0
+    # if subreddit is None or type(subreddit) is not str:
+    #     return 0
     response = requests.get(url, headers=headers, allow_redirects=False)
+    # print(response.status_code)
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
+        json_data = response.json()
+        data_results = json_data.get("data")
+        subscribers = data_results.get("subscribers")
         return subscribers
     else:
         return 0
