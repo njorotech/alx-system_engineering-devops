2311754a512832be7945bcc454f71efcef69a784
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-03-11 11:28:39 +0300

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a file
 file { 'school':
-  path => '/tmp/school',
+  path    => '/tmp/school',
   content => 'I love Puppet',
-  mode => '0744',
-  group => 'www-data',
-  owner => 'www-data'
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data'
 }
