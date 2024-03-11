6b33ee9ceea9eea145ef02cfb3641953010fd873
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-03-11 12:06:39 +0300

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install a package
 package { 'flask':
-  ensure            => '2.1.0',
-  provider          => 'pip3',
-  install_options   => ['--allow-downgrades'],
+  ensure          => '2.1.0',
+  provider        => 'pip3',
+  install_options => ['--allow-downgrades'],
 }
